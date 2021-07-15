0942039d3b3fe9b83c9a66f1fc03222697b0a79a
Las Safin <las@protonmail.ch>
2021-07-15 10:53:46 +0000

    Procex.Shell

---
+++ Procex/Shell.hs
@@ -0,0 +1,20 @@
+module Procex.Shell (promptFunction, initInteractive) where
+
+import System.Posix.Env
+import System.Posix.Directory
+import System.IO
+
+promptFunction :: [String] -> Int -> IO String
+promptFunction _modules _line = do
+  d <- getEnvDefault "PWD" ""
+  -- For some ungodly reason, cd-ing inside ghci won't change the cwd of ghci itself,
+  -- so completion, etc. will always happen from the directory you started ghci in.
+  -- This is a quick hack to work around this by also running changeWorkingDirectory
+  -- "above" the shell.
+  changeWorkingDirectory d
+  -- This is just what the prompt will show inside ghci.
+  pure $ d <> ": "
+
+-- | You need to run this if you want stdin to work properly inside ghci.
+initInteractive :: IO ()
+initInteractive = hSetBuffering stdin LineBuffering
