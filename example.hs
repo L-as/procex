182892a49ed768c12354789d654b412f53224cb6
Las Safin <las@protonmail.ch>
2021-07-23 18:46:33 +0000

    Use Hspec for testing

--- example.hs
+++ example.hs
@@ -14,11 +14,24 @@ import System.Exit
 import Control.Concurrent.Async (wait)
 import System.Posix.Process
 import Control.Monad
+import Test.Hspec
+import qualified Data.ByteString.Lazy as B
 
 main :: IO ()
-main = do
-  mq "diff" (pipeArgStrIn "ab\ncd") (pipeArgStrIn "ab\ncd")
-  _ <- wait <=< run' $ mq "diff" (pipeArgStrIn "ab\ncd") (pipeArgStrIn "ab\nce")
-  try (mq "false") >>= \case
-    Left (CmdException (Exited (ExitFailure 1))) -> pure ()
-    x -> fail $ "unreachable: " <> show x
+main = hspec $ do
+  describe "capture" $ do
+    it "captures stdout" $ capture (mq "echo" "xjjj") `shouldReturn` "xjjj\n"
+
+  describe "captureLazy" $ do
+    it "captures stdout lazily" $ do
+      out <- captureLazy $ mq "cat" "/dev/zero"
+      B.take 16 out `shouldBe` B.pack (take 16 $ repeat 0)
+
+  describe "captureLazyNoThrow" $ do
+    it "captures stdout lazily without throwing" $ do
+      out <- captureLazyNoThrow $ mq "diff" (pipeArgStrIn "ab\ncd") (pipeArgStrIn "ab\nce")
+      out `shouldBe` "2c2\n< cd\n\\ No newline at end of file\n---\n> ce\n\\ No newline at end of file\n"
+
+  describe "run" $ do
+    it "throws when cmd fails" $ do
+      run (mq "false") `shouldThrow` \(_ :: CmdException) -> True
--- procex.nix
+++ procex.nix
@@ -1,5 +1,5 @@
 { mkDerivation, async, base, bytestring, containers, lib, unix
-, utf8-string, replace-megaparsec, directory, deepseq
+, utf8-string, replace-megaparsec, directory, deepseq, hspec
 }:
 mkDerivation {
   pname = "procex";
@@ -10,7 +10,7 @@ mkDerivation {
     async base bytestring containers unix utf8-string deepseq
   ];
   testHaskellDepends = [
-    async base replace-megaparsec unix directory
+    async base replace-megaparsec unix directory hspec
   ];
   license = lib.licenses.asl20;
   hydraPlatforms = lib.platforms.none;
